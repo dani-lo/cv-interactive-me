7420472035fe2d52d46b7109fd935ec26308131d
dani longo <resina00@gmail.com>
2023-11-25 12:47:11 +0530

    change to rust wasm app env var

--- cv_rust_wasm/src/api/config.rs
+++ cv_rust_wasm/src/api/config.rs
@@ -31,9 +31,10 @@ pub fn get_static_api_config () -> HashMap<CvStaticEndpoints, &'static str>  {
 
     let mut conf: HashMap<CvStaticEndpoints, &'static str> = HashMap::new();
 
-    let release = env::var("INTERACTIVEME_RELEASE").is_ok();
+    let dev = env::var("INTERACTIVEME_DEV").is_ok();
 
-    if !release {
+    
+    if dev {
         conf.insert(CvStaticEndpoints::JOB, "http://localhost:8080/api/jobs");
         conf.insert(CvStaticEndpoints::COMPANY, "http://localhost:8080/api/companies");
         conf.insert(CvStaticEndpoints::TECH, "http://localhost:8080/api/techs");
@@ -48,13 +49,6 @@ pub fn get_static_api_config () -> HashMap<CvStaticEndpoints, &'static str>  {
         conf.insert(CvStaticEndpoints::JOBTYPES, "https://xbhydccz1j.execute-api.eu-west-2.amazonaws.com/Prod/jobtypes");
         conf.insert(CvStaticEndpoints::PROJECTS, "https://xbhydccz1j.execute-api.eu-west-2.amazonaws.com/Prod/projects");
     }
-
-    // conf.insert(CvStaticEndpoints::JOB, "https://xbhydccz1j.execute-api.eu-west-2.amazonaws.com/Prod/jobs");
-    // conf.insert(CvStaticEndpoints::COMPANY, "https://xbhydccz1j.execute-api.eu-west-2.amazonaws.com/Prod/companies");
-    // conf.insert(CvStaticEndpoints::TECH, "https://xbhydccz1j.execute-api.eu-west-2.amazonaws.com/Prod/tech");
-    // conf.insert(CvStaticEndpoints::FIELDS, "https://xbhydccz1j.execute-api.eu-west-2.amazonaws.com/Prod/fields");
-    // conf.insert(CvStaticEndpoints::JOBTYPES, "https://xbhydccz1j.execute-api.eu-west-2.amazonaws.com/Prod/jobtypes");
-    // conf.insert(CvStaticEndpoints::PROJECTS, "https://xbhydccz1j.execute-api.eu-west-2.amazonaws.com/Prod/projects");
     
     conf
 }
@@ -63,24 +57,19 @@ pub fn get_actions_api_config () -> HashMap<CvActionsEndpoints, &'static str>  {
 
     let mut conf: HashMap<CvActionsEndpoints, &'static str> = HashMap::new();
 
-    let release = env::var("INTERACTIVEME_RELEASE").is_ok();
+    let dev = env::var("INTERACTIVEME_DEV").is_ok();
 
-    if release {
-        conf.insert(CvActionsEndpoints::BOOKMARKS, "https://interactiveme.net/api/bookmarks");
-        conf.insert(CvActionsEndpoints::FILTERS, "https://interactiveme.net/api/filters");
-        conf.insert(CvActionsEndpoints::ANNOTATIONS, "https://interactiveme.net/api/annotations");
-        conf.insert(CvActionsEndpoints::USER, "https://interactiveme.net/api/users");
-    } else {
+    if dev {
         conf.insert(CvActionsEndpoints::BOOKMARKS, "http://localhost:8000/api/bookmarks");
         conf.insert(CvActionsEndpoints::FILTERS, "http://localhost:8000/api/filters");
         conf.insert(CvActionsEndpoints::ANNOTATIONS, "http://localhost:8000/api/annotations");
         conf.insert(CvActionsEndpoints::USER, "http://localhost:8000/api/users");
+    } else {
+        conf.insert(CvActionsEndpoints::BOOKMARKS, "https://interactiveme.net/api/bookmarks");
+        conf.insert(CvActionsEndpoints::FILTERS, "https://interactiveme.net/api/filters");
+        conf.insert(CvActionsEndpoints::ANNOTATIONS, "https://interactiveme.net/api/annotations");
+        conf.insert(CvActionsEndpoints::USER, "https://interactiveme.net/api/users");
     }
-
-    // conf.insert(CvActionsEndpoints::BOOKMARKS, "http://localhost:8000/api/bookmarks");
-    //     conf.insert(CvActionsEndpoints::FILTERS, "http://localhost:8000/api/filters");
-    //     conf.insert(CvActionsEndpoints::ANNOTATIONS, "http://localhost:8000/api/annotations");
-    //     conf.insert(CvActionsEndpoints::USER, "http://localhost:8000/api/users");
 
     conf
 }
